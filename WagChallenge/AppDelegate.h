3423955e0fc639632a7b6a150a13b7f094e96eae
William Morriss <wjmelements@gmail.com>
2018-04-10 17:20:05 -0700

    Initial Commit

---
+++ WagChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WagChallenge
+//
+//  Created by William Morriss on 4/10/18.
+//  Copyright © 2018 William Morriss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WagChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WagChallenge
+//
+//  Created by William Morriss on 4/10/18.
+//  Copyright © 2018 William Morriss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
