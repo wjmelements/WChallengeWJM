5dc62328cab1b57a2ace47dff346ec83332795ef
William Morriss <wjmelements@gmail.com>
2018-04-11 17:45:49 -0700

    LoadingView

--- WagChallenge/UserCell.m
+++ WagChallenge/UserCell.m
@@ -1,9 +1,40 @@
 #import "UserCell.h"
 #import "StackExchange.h"
 
+static CGFloat kLabelPadding = 7.0f;
 
 @implementation UserCell {
     NSString *_imageHash;
+    UIActivityIndicatorView *_loadingView;
+}
+
+- (void)showLoadingView {
+    if (!_loadingView) {
+        _loadingView = [[UIActivityIndicatorView alloc]
+            initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray
+        ];
+        [_loadingView startAnimating];
+    }
+    [self.contentView addSubview:_loadingView];
+}
+
+- (void)hideLoadingView {
+    [_loadingView removeFromSuperview];
+    [_loadingView stopAnimating];
+    _loadingView = nil;
+}
+
+- (void)layoutSubviews {
+    CGRect bounds = self.contentView.bounds;
+    CGFloat height = CGRectGetHeight(bounds);
+    CGFloat width = CGRectGetWidth(bounds);
+    CGRect leftFrame = CGRectMake(0, 0, height, height);
+    if (_loadingView) {
+        _loadingView.frame = leftFrame;
+    }
+    self.imageView.frame = leftFrame;
+    CGFloat labelLeft = height + kLabelPadding;
+    self.textLabel.frame = CGRectMake(labelLeft, 0, width - labelLeft, height);
 }
 
 - (void)loadImage:(NSString *)remoteURL hash:(NSString *)hash {
@@ -15,14 +46,16 @@
         UIImage *image = [UIImage imageWithContentsOfFile:cachedFile];
         self.imageView.image = image;
     } else {
+        [self showLoadingView];
         dispatch_async(networkQueue, ^{
             NSLog(@"Fetching %@", remoteURL);
             NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:remoteURL]];
             UIImage *image = [UIImage imageWithData:data];
             dispatch_async(dispatch_get_main_queue(), ^{
                 if ([hash isEqualToString:self->_imageHash]) {
+                    [self hideLoadingView];
                     self.imageView.image = image;
-                    [self layoutSubviews];
+                    //[self layoutSubviews];
                 } else {
                     NSLog(@"Hash has changed:%@ -> %@", hash, self->_imageHash);
                 }
